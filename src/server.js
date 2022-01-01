561edc9738a4db24aa64af4be443bad985af2e8b
정민혁 <chmh0805@gmail.com>
2022-01-01 12:54:12 +0900

    8. ChatApp Using WebSocket - part 5, Make to able to access Admin Console

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
-import SocketIO from 'socket.io';
+import { Server } from 'socket.io';
+import { instrument } from '@socket.io/admin-ui';
 
 const app = express();
 
@@ -20,7 +21,16 @@ const handleListen = () => {
 };
 
 const httpServer = http.createServer(app); // Create Http Server With Express.js
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ['https://admin.socket.io'],
+    credentials: true,
+  },
+});
+
+instrument(wsServer, {
+  auth: false,
+});
 
 function publicRooms() {
   const {
